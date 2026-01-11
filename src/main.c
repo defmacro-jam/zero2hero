bdeaf900eafaeb4bcb44dabef4f6985620cb00a5
Michael Bentley <michael@patchwork.ai>
2026-01-11 13:28:22 -0500

    command line arguments

--- src/main.c
+++ src/main.c
@@ -8,8 +8,43 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t-n - create new database file\n");
+  printf("\t-f - (required) path to database file\n");
+
+  return;
 }
 
-int main(int argc, char *argv[]) { 
-	
+int main(int argc, char *argv[]) {
+  char *filepath = NULL;
+  bool newfile = false;
+	int c;
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch(c) {
+    case 'f':
+      filepath = optarg;
+      break;
+    case 'n':
+      newfile = true;
+      break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+      break;
+    default:
+      return -1;
+    }
+  }
+
+  if(filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+
+    return 0;
+  }
+
+  printf("Newfile: %d\n", newfile);
+  printf("Filepath: %s\n", filepath);
+
+  return 0;
 }
